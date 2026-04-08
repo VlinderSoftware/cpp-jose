b9ad61067bf58216f36d63548e5a627c55652d14
Ronald Landheer-Cieslak <rlc@vlinder.ca>
2026-04-07 21:29:15 -0400

    fix: use span instead of std::span in BackEnd::sign for consistency

--- src/private/back_end.cpp
+++ src/private/back_end.cpp
@@ -113,7 +113,7 @@ BackEnd::concatKDF(vector<unsigned char> const &shared_secret /* Z in the spec *
 vector<unsigned char>
 BackEnd::sign(SignatureAlgorithm algorithm, JWK const &key, vector<unsigned char> const &data) const
 {
-    return sign(algorithm, key, std::span<unsigned char const>(data.data(), data.size()));
+    return sign(algorithm, key, span<unsigned char const>(data.data(), data.size()));
 }
 
 vector<unsigned char> BackEnd::sign(SignatureAlgorithm algorithm,
